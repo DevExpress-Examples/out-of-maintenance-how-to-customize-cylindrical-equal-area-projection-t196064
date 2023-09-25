df4809f86e8ea315175f5602ad46dbd72628194e
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 19:35:13 +0400

    Change example status to out of maintenance

--- VB/VB_MapControl_CustomProjections/ProjectionRatio.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class ProjectionRatio
-    Public Sub New(name As String, value As String)
-        Me.Name = name
-        Me.Value = value
-    End Sub
-
-    Public Property Name() As String
-    Public Property Value() As Double
-End Class
